a5532e3a51286dde03814462d6e6e01d7bac4e94
Jeff Lu <cl3883@columbia.edu>
2020-07-01 22:21:03 +0800

    changed to rolling average

--- corona_dash_live.py
+++ corona_dash_live.py
@@ -94,7 +94,6 @@ def jhu_data_process_day(date_df):
     day_df.index += 1
     return day_df
 
-
 def jhu_data_process_2day_specific(date_df, day_threshold):
     country_arrays = [np.array(date_df[c])[np.array(date_df[c]) > day_threshold] for c in date_df.columns]
     day_df = pd.DataFrame(country_arrays).T
@@ -110,6 +109,18 @@ def jhu_data_process_2day_specific(date_df, day_threshold):
                   for n in range(1, len(daily_increase_df)) if n % 2 == 1] for c in daily_increase_df.columns]
     avg_2day_increase_df = pd.DataFrame(list_list, index=list(daily_increase_df.columns)).T
     return avg_2day_increase_df
+
+def jhu_data_process_7day_moving(date_df, day_threshold):
+    country_arrays = [np.array(date_df[c])[np.array(date_df[c]) > day_threshold] for c in date_df.columns]
+    day_df = pd.DataFrame(country_arrays).T
+    day_df.columns = list(date_df.columns)
+    day_df.index += 1
+    # calculate daily increase
+    temp_df = day_df.diff(periods=1)
+    series_list = [temp_df[c].shift(-1)[temp_df[c].shift(-1) > 0].reset_index(drop=True) for c in temp_df.columns]
+    daily_increase_df = pd.DataFrame(series_list).T
+    daily_increase_df.index = daily_increase_df.index + 1
+    return daily_increase_df.rolling(window=7).mean().shift(-6)
 
 
 ## Population data
@@ -137,6 +148,8 @@ def data_process_avg(complete_df):
 
 ## final dataset
 infection_data = data_process_avg(jhu_data_process_2day_specific(jhu_data_process_date(df), 100))
+# moving average
+infection_data_moving = data_process_avg(jhu_data_process_7day_moving(jhu_data_process_date(df), 100))
 
 
 ## Top 10 countries
@@ -185,7 +198,7 @@ country_options4 = [dict(label=c, value=c) for c in sorted(meast_country)]
 dropdown_function1 = dcc.Dropdown(id='country_selection1',
                                   options=country_options1,
                                   multi=True,
-                                  value=['Singapore'],
+                                  value=['India'],
                                   placeholder="Asian Countries",
                                   # className="dcc_control"
                                   )
@@ -209,7 +222,7 @@ dropdown_function3 = dcc.Dropdown(id='country_selection3',
 dropdown_function4 = dcc.Dropdown(id='country_selection4',
                                   options=country_options4,
                                   multi=True,
-                                  value=['Iran'],
+                                  value=[],
                                   placeholder="Middle East Countries",
                                   # className="dcc_control"
                                   )
@@ -235,12 +248,12 @@ app.layout = html.Div([
     html.Div(className="row", children=[
         html.Div(className='four columns', children=[
             html.H2('Spot the Curve: COVID-19', style={'font-weight': 'bold', "margin-top": "10px", "margin-bottom": "2px"}),
-            html.H6('Visualizing the Pressure of Healthcare with Case Data', style={'color': '#6094B3', 'font-weight': 'bold', "margin-top": "1px", "margin-bottom": "0px"}),
+            html.H6('Visualizing the Pressure of Healthcare', style={'color': '#6094B3', 'font-weight': 'bold', "margin-top": "1px", "margin-bottom": "0px"}),
             html.Div(['Made by ', html.A('Jeff Lu', href='https://www.linkedin.com/in/jefflu-chia-ching-lu/')], style={'font-style': 'italic', 'display': 'inline', "margin-bottom": "10px"}),
             # html.Br(),
             html.P(
                 '''
-                To understand the current pressure of healthcare capacity in each country,  
+                To understand the current pressure upon healthcare system in each country,  
                 this app visualizes the latest confirmed cases of Coronavirus (COVID-19). 
                 Pick the countries that you are interested in and see if you can spot the CURVE from them.
                 ''',
@@ -250,9 +263,9 @@ app.layout = html.Div([
                 '''
                 Assuming the capacity of healthcare system is correlated to the population of the country 
                 (and other health-resource-related data such as the number of hospital bed don't have consistency 
-                across countries), I made 
-                the metric here as the “Infection Rate” (the number of confirmed cases per 1 million people).
-                The number of daily increase is also aggregated into 2-day format to smooth trends. 
+                across countries), I made the metric here as the “Infection Rate” (the number of confirmed cases per 
+                1 million people) to showcase the curve.
+                The number of daily increase is also aggregated into 7-day rolling average format to smooth trends. 
                 '''
             ),
             html.P('Top 10 Highest-Infection-Rate Countries', style={'font-weight': 'bold'}),
@@ -285,7 +298,8 @@ app.layout = html.Div([
             html.P([dropdown_function2]),
             html.P([dropdown_function3]),
             html.P([dropdown_function4]),
-            html.Button(id='submit-button-state', n_clicks=0, children='Submit'),
+            html.Button(id='button', n_clicks=0, children='Submit'),
+            # html.Button(id='button2', n_clicks=0, children='7-Day Rolling Average', style={"margin-left": "10px"}),
             dcc.Graph(id='output-graph', animate=None),
             html.P(
                 ['Data Source: ', html.A('Johns Hopkins University', href='https://github.com/CSSEGISandData/COVID-19'),
@@ -313,8 +327,61 @@ app.layout = html.Div([
 ## p.s. they have different components so watch out (children vs figure)
 
 ### plotting function ###
+#### Legacy plotting function
 
-def create_trend_line_infection_rate_2day(df, country_list, title_name):
+# def create_trend_line_infection_rate_2day(df, country_list, title_name):
+#     fig = go.Figure()
+#     fig.layout.template = 'ggplot2'
+#     for c in country_list:
+#         fig.add_trace(go.Scatter(x=df[c].dropna().index, y=df[c].dropna().values,
+#                                  mode='lines',
+#                                  # line_shape='spline',
+#                                  name=c,
+#                                  # hoverinfo="y+name",
+#                                  hovertemplate='<b>%{y:.0f}</b> new cases out of 1M people',))
+#     fig.update_xaxes(title_text='2-Day (Start on the date with the 100th case in each country)')
+#     fig.update_yaxes(title_text='Increased Positive Cases per 1 million people')
+#     fig.update_layout(
+#         title={
+#             'text': title_name,
+#             'font': {'size': 22, 'family': 'Arial, sans-serif'},
+#             'y': 0.95,
+#             #         'x':0.5,
+#             'xanchor': 'center',
+#             'yanchor': 'top'},
+#         margin=dict(l=20, r=20, t=50, b=20),
+#         xaxis={
+#             'tickmode': 'array',
+#             'ticks': 'outside',
+#             'tickcolor': '#F7FBFE',
+#             'title_standoff': 15,
+#             # 'tick0': 2, # seems not supporting for now
+#             # 'dtick': 2,
+#             'tickvals': [n for n in range(0, 264) if (n + 2) % 2 == 0],  # Infinite number
+#             'ticktext': [ordinal(n) for n in [n for n in range(2, 528) if (n + 2) % 4 == 0]],  # Infinite number
+#         },
+#         yaxis={'title_standoff': 5, 'ticks': 'outside', 'tickcolor': '#F7FBFE', 'gridcolor': '#EEEEEE'},
+#         xaxis_showgrid=False,
+#         paper_bgcolor='#F7FBFE',  # canvas color
+#         plot_bgcolor='#F7FBFE', # plot color #D8D991 #F6EEDF #FFF8DE
+#         hoverlabel={'namelength':-1},
+#
+#     )
+#     # fig.update_layout(hovermode="y")
+#     ## color
+#
+#     ## new
+#     color_n = len(country_list)
+#     # cm = plt.get_cmap('tab10')
+#     # for i, color in enumerate([mpl.colors.rgb2hex(cm(1. * i / color_n)) for i in range(color_n)]):
+#     cm = px.colors.qualitative.T10 # e.g. ['#4C78A8']
+#     for i, color in enumerate(cm[0:color_n]):
+#         fig['data'][i]['marker'].update({'color': color})
+#         fig['data'][i]['marker']['line'].update({'color': color})
+#     return fig
+
+## 7-day moving average
+def create_trend_line_infection_rate_7day_moving(df, country_list, title_name):
     fig = go.Figure()
     fig.layout.template = 'ggplot2'
     for c in country_list:
@@ -324,7 +391,7 @@ def create_trend_line_infection_rate_2day(df, country_list, title_name):
                                  name=c,
                                  # hoverinfo="y+name",
                                  hovertemplate='<b>%{y:.0f}</b> new cases out of 1M people',))
-    fig.update_xaxes(title_text='2-Day (Start on the date with the 100th case in each country)')
+    fig.update_xaxes(title_text='1-Day (Start on the date with the 100th case in each country)')
     fig.update_yaxes(title_text='Increased Positive Cases per 1 million people')
     fig.update_layout(
         title={
@@ -342,8 +409,8 @@ def create_trend_line_infection_rate_2day(df, country_list, title_name):
             'title_standoff': 15,
             # 'tick0': 2, # seems not supporting for now
             # 'dtick': 2,
-            'tickvals': [n for n in range(0, 264) if (n + 2) % 2 == 0],  # Infinite number
-            'ticktext': [ordinal(n) for n in [n for n in range(2, 528) if (n + 2) % 4 == 0]],  # Infinite number
+            'tickvals': [n for n in range(0, 264) if (n + 10) % 10 == 0],  # Infinite number
+            'ticktext': [ordinal(n) for n in range(0, 264) if (n + 10) % 10 == 0],  # Infinite number
         },
         yaxis={'title_standoff': 5, 'ticks': 'outside', 'tickcolor': '#F7FBFE', 'gridcolor': '#EEEEEE'},
         xaxis_showgrid=False,
@@ -366,11 +433,44 @@ def create_trend_line_infection_rate_2day(df, country_list, title_name):
     return fig
 
 
+
 # the output's component_property was 'children'
+
+# @app.callback(
+#     Output(component_id='output-graph', component_property='figure'),
+#     [Input('button1', 'n_clicks_timestamp')],
+#     [State(component_id='country_selection1', component_property='value'),
+#      State(component_id='country_selection2', component_property='value'),
+#      State(component_id='country_selection3', component_property='value'),
+#      State(component_id='country_selection4', component_property='value')]
+# )
+# # State use for click action required!
+#
+# # if you want to change by input, then plotting should be in the def at the end ("input_data")
+# # if you only want to show specific graph, then can plot it in the app.layout
+#
+# ## Multiple dropdown list
+# def goplot(button_1, country_selection1, country_selection2, country_selection3, country_selection4):
+#     # make sure it won't generate blank plot by itself and show error
+#     if not (country_selection1, country_selection2, country_selection3, country_selection4):
+#         return no_update  # This is prevent the web run the function without any input
+#     # elif button_1:
+#     #     outputtt = create_trend_line_infection_rate_2day(infection_data, country_selection1 + country_selection2
+#     #                                                      + country_selection3 + country_selection4,
+#     #                                                      '<b>Infection Rate Per Country</b>')
+#     else:
+#         outputtt = create_trend_line_infection_rate_2day(infection_data, country_selection1 + country_selection2
+#                                                          + country_selection3 + country_selection4,
+#                                                          '<b>Infection Rate Per Country</b>')
+#     return outputtt
+        # return country_selection
+
+#### test multiple buttons
 
 @app.callback(
     Output(component_id='output-graph', component_property='figure'),
-    [Input('submit-button-state', 'n_clicks')],
+    # [Input('button1', 'n_clicks'), Input('button2', 'n_clicks')],
+    [Input('button', 'n_clicks')],
     [State(component_id='country_selection1', component_property='value'),
      State(component_id='country_selection2', component_property='value'),
      State(component_id='country_selection3', component_property='value'),
@@ -382,19 +482,26 @@ def create_trend_line_infection_rate_2day(df, country_list, title_name):
 # if you only want to show specific graph, then can plot it in the app.layout
 
 ## Multiple dropdown list
-def goplot(n_clicks, country_selection1, country_selection2, country_selection3, country_selection4):
+def goplot2(button, country_selection1, country_selection2, country_selection3, country_selection4):
     # make sure it won't generate blank plot by itself and show error
     if not (country_selection1, country_selection2, country_selection3, country_selection4):
         return no_update  # This is prevent the web run the function without any input
+    # elif button_1 > button_2:
+    #     outputtt = create_trend_line_infection_rate_2day(infection_data, country_selection1 + country_selection2
+    #                                                      + country_selection3 + country_selection4,
+    #                                                      '<b>Infection Rate Per Country (2-Day Aggregation)</b>')
+    # elif button_2:
+    #     outputtt = create_trend_line_infection_rate_7day_moving(infection_data_moving, country_selection1 + country_selection2
+    #                                                        + country_selection3 + country_selection4,
+    #                                                        '<b>Infection Rate Per Country (7-Day Rolling Average)</b>')
     else:
-        outputtt = create_trend_line_infection_rate_2day(infection_data, country_selection1 + country_selection2
-                                                         + country_selection3 + country_selection4,
-                                                         '<b>Infection Rate Per Country</b>')
-        return outputtt
-        # return country_selection
-
+        outputtt = create_trend_line_infection_rate_7day_moving(infection_data_moving,
+                                                                country_selection1 + country_selection2
+                                                                + country_selection3 + country_selection4,
+                                                                '<b>Infection Rate Per Country (7-Day Rolling Average)</b>')
+        # return no_update
+    return outputtt
 
 # n_clicks is required for click event
-
 if __name__ == '__main__':
     app.run_server(debug=True)
